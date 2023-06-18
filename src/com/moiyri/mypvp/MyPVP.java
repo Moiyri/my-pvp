1031a932772032416864a841264d9792956ee49d
Moiyri <moiyri@163.com>
2023-06-18 11:38:27 +0800

    initial plugin and delete main

---
+++ src/com/moiyri/mypvp/MyPVP.java
@@ -0,0 +1,16 @@
+package com.moiyri.mypvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPVP extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
